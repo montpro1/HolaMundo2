2326d71eb709a3809b7e8d024f3671c2d82993b0
DANIELCASTELAO\vmontestovar <vmontestovar@danielcastelao.org>
2019-11-29 10:57:54 +0100

    second commit

--- holamundo2/HolaMundo2.java
+++ holamundo2/HolaMundo2.java
@@ -15,7 +15,8 @@ public class HolaMundo2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("holamundo2");
+        System.out.println("holamundo223");
+        //holaquetal
     }
     
 }
